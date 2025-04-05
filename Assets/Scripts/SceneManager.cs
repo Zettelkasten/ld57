27d25221fac720d84d957a2555f125492cce8f67
anton.florey <anton.florey@googlemail.com>
2025-04-05 20:46:28 +0200

    blub

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,17 +1,38 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class SceneManager : MonoBehaviour
+
+public class GlobalSceneManager : MonoBehaviour
 {
+    private int currentSceneIndex = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+		currentSceneIndex = 0;
+		DontDestroyOnLoad(gameObject);
+	}
 
+    public void LoadNextScene()
+    {
+        currentSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCount;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+	public void LoadMainMenu()
+	{
+        currentSceneIndex = 0;
+		SceneManager.LoadScene(0);
+	}
+
+    public void StartGame()
     {
-        
+        currentSceneIndex = 1;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit the game now!");
+        Application.Quit();
     }
 }
---
+++ Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    // Audio players components.
+    public AudioSource EffectsSource;
+    public AudioSource MusicSource;
+
+    // Singleton instance.
+    public static SoundManager Instance = null;
+
+    // Initialize the singleton instance.
+    private void Awake()
+    {
+        // If there is not already an instance of SoundManager, set it to this.
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        //If an instance already exists, destroy whatever this object is to enforce the singleton.
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Play a single clip through the sound effects source.
+    public void Play(AudioClip clip, float pitch = 1)
+    {
+        EffectsSource.pitch = pitch;
+        EffectsSource.clip = clip;
+        EffectsSource.Play();
+    }
+
+    // Play a single clip through the music source.
+    public void PlayMusic(AudioClip clip)
+    {
+        MusicSource.clip = clip;
+        MusicSource.Play();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        MusicSource.volume = volume;
+    }
+
+    public void SetSoundVolume(float volume)
+    {
+        EffectsSource.volume = volume;
+    }
+
+
+}
