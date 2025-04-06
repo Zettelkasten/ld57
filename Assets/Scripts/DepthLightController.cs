602f66ab316262df577738a594ff3b8a1fd2553c
anton.florey <anton.florey@googlemail.com>
2025-04-07 00:17:35 +0200

    sky

--- Assets/Scripts/DepthLightController.cs
+++ Assets/Scripts/DepthLightController.cs
@@ -8,12 +8,16 @@ public class DepthLightController : MonoBehaviour
 
     public Light2D sceneLight;
 
+    public Light2D playerAuraLight;
+
     private float maxSceneLightStrength = 1.0f;
+    private float maxPlayerAuraLightStrength = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         maxSceneLightStrength = sceneLight.intensity;
+        maxPlayerAuraLightStrength = playerAuraLight.intensity;
     }
 
     // Update is called once per frame
@@ -21,5 +25,8 @@ public class DepthLightController : MonoBehaviour
     {
         float depthFactor = Mathf.Clamp((transform.position.y - darkestDepth) / (oceanSurface - darkestDepth), 0, 1);
         sceneLight.intensity = Mathf.Lerp(0, maxSceneLightStrength, depthFactor);
+
+		float playerAuroFactor = Mathf.Clamp((oceanSurface - 30 - transform.position.y) / 50, 0, 1);
+        playerAuraLight.intensity = Mathf.Lerp(0, maxPlayerAuraLightStrength, playerAuroFactor);
 	}
 }
