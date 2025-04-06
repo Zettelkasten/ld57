b8ed8b87498f6cd5f816047b54d6beb453d9ad40
David Causin <stimmts@gmx.net>
2025-04-06 02:43:41 +0200

    better player uprighting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,7 +8,7 @@ public class Player : MonoBehaviour
     Rigidbody2D playerRigidbody;
     public Vector2 armPosition = new Vector2(0, 0.45f);
     public PolygonCollider2D gearCollider;
-    private float speed = 5f;
+    private float speed = 8f;
 
     private HingeJoint2D joint1;
     Vector2 joint1Pos;
@@ -43,6 +44,13 @@ public class Player : MonoBehaviour
         joint1.anchor = joint1Pos;
         joint2.anchor = joint2Pos;
         joint3.anchor = joint3Pos;
+    }
+
+    private void FixedUpdate()
+    {
+        // upright the player a bit
+        playerRigidbody.angularVelocity *= 0.99f;
+        playerRigidbody.angularVelocity -= playerRigidbody.rotation * 0.1f;
     }
 
     void Update()
