d84472d5cf34e7a20a5583193706190fb6626b43
David Causin <stimmts@gmx.net>
2025-04-07 04:36:23 +0200

    aSGFŴASDFÖOLKAWSMRÜOGHIJKQ3EWRÜPHGOI0JK

---
+++ Assets/Scripts/Lift.cs
@@ -0,0 +1,84 @@
+using System;
+using UnityEngine;
+
+public class Lift : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D liftRigidbody;
+    public Rigidbody2D platformRigidbody;
+    GameObject platform;
+    private Vector2 up_pos;
+    public Transform target;
+    public float maxspeed = 10f;
+    public float height;
+    private float speed;
+    public int direction;
+    bool is_up = true;
+    private float descenttime;
+    private float descenttimer = 0f;
+    
+    
+    
+    
+    void Start()
+    {
+        platform = platformRigidbody.gameObject;
+        height = platform.transform.position.y - target.position.y;
+        descenttime = height / maxspeed * 2;
+    }
+
+    float get_progress()
+    {
+        return up_pos.y - platform.transform.position.y;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if (direction == 0){
+                // start moving
+                if (is_up)
+                {
+                    direction = -1;
+                }
+                else
+                {
+                    direction = 1;
+                }
+                descenttimer = 0f;
+                
+            }
+        }
+        if(direction != 0)
+        {
+            descenttimer += Time.deltaTime;
+            if (descenttimer >= descenttime)
+            {
+                if(direction == 1)
+                {
+                    is_up = true;
+                }
+                else
+                {
+                    is_up = false;
+                }
+                direction = 0;
+                descenttimer = 0f;
+                speed = 0f;
+            }
+            else
+            {
+                float progress = descenttimer/descenttime;
+                progress *= 2;
+                if (progress > 1)
+                {
+                    progress = 2 - progress;
+                }
+                speed = maxspeed * progress;
+            }
+            liftRigidbody.linearVelocity = new Vector2(0, speed * direction);
+        }
+    }
+}
