81f73e5eee81bbfef0128f937f19e411aba62e6f
anton.florey <anton.florey@googlemail.com>
2025-04-08 00:17:27 +0200

    Cooler Bild is DA

--- Assets/Scripts/HackyParallax.cs
+++ Assets/Scripts/HackyParallax.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 public class HackyParallax : MonoBehaviour
 {
     public bool onlyX = true;
-    public bool worldOrigAsAnchor = true;
+    public bool selfAsAnchor = true;
     public Transform parallaxCamera;
     public Transform parallaxAnchor;
     public Vector2 yLimit = new Vector2(-10000, 10000);
     public Vector2 xLimit = new Vector2(-10000, 10000);
 
     private Vector3 myStartPos = Vector3.zero;
+    private Vector3 anchorPos = Vector3.zero;
     public float parallaxFactor = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         myStartPos = transform.localPosition;
+        anchorPos = (selfAsAnchor ? transform.localToWorldMatrix * myStartPos : parallaxAnchor.position);
 	}
 
     // Update is called once per frame
     void LateUpdate()
     {
-		Vector3 anchorToCam = parallaxCamera.position - (worldOrigAsAnchor ? Vector3.zero : parallaxAnchor.position);
+		Vector3 anchorToCam = parallaxCamera.position - anchorPos;
         // set position with parallax effect
         Vector3 shift;
         if (onlyX)
