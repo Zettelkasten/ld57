3571762915459c2146fc6b5c00c3f0fe69d69dba
Frithjof <fmpetrick@gmail.com>
2025-04-07 02:28:10 +0200

    fancy cam

--- Assets/Scripts/Cage.cs
+++ Assets/Scripts/Cage.cs
@@ -29,6 +29,8 @@ public class Cage : MonoBehaviour
     public float sinkSpeed;
     private List<Rigidbody2D> sinkingAttachedObjects;
     
+    private Transform cameraPosition;
+    
     // selling items
     private float sellingItemProgress;
     public float sellingItemSpeed;
@@ -90,6 +92,14 @@ public class Cage : MonoBehaviour
                     sinkProgress = 0.7f;
                 }
 
+                if (state == CageState.Sinking)
+                {
+                    // instantiate a dummy object
+                    cameraPosition = new GameObject("CameraPosition").transform;
+                    cameraPosition.position = World.Instance.player.transform.position;
+                    World.Instance.cinemachineVirtualCamera.Follow = cameraPosition;
+                }
+
                 // Disable the ship collider
                 World.Instance.aboveSea.shipCollider.enabled = false;
                 break;
@@ -122,8 +132,25 @@ public class Cage : MonoBehaviour
 
                 sinkProgress += Time.fixedDeltaTime * sinkSpeed;
                 this.transform.position = Vector3.Lerp(fromPos, toPos, Helpers.EaseInOutQuad(sinkProgress));
+
+                if (cameraPosition != null)
+                {
+                    // until 0.3, don't move it, then move it to the player
+                    if (sinkProgress >= 0.3f)
+                    {
+                        cameraPosition.position = Vector3.Lerp(cameraPosition.position, World.Instance.player.transform.position, Helpers.EaseInOutQuad((sinkProgress - 0.3f) / 0.7f));
+                    }
+                }
+                
                 if (sinkProgress >= 1)
                 {
+                    World.Instance.cinemachineVirtualCamera.Follow = World.Instance.player.transform;
+                    if (cameraPosition != null)
+                    {
+                        Destroy(cameraPosition.gameObject);
+                        cameraPosition = null;
+                    }
+
                     if (state != CageState.SinkingWithoutPlayer)
                     {
                         // make the player a child of the game scene
