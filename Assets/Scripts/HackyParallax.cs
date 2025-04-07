e1bc7b29eec388f7c4fce01a13ac8d147d132f7b
anton.florey <anton.florey@googlemail.com>
2025-04-07 16:26:32 +0200

    parallax

---
+++ Assets/Scripts/HackyParallax.cs
@@ -0,0 +1,40 @@
+using UnityEditor.UI;
+using UnityEngine;
+
+public class HackyParallax : MonoBehaviour
+{
+    public bool onlyX = true;
+    public bool worldOrigAsAnchor = true;
+    public Transform parallaxCamera;
+    public Transform parallaxAnchor;
+    public Vector2 yLimit = new Vector2(-10000, 10000);
+    public Vector2 xLimit = new Vector2(-10000, 10000);
+
+    private Vector3 myStartPos = Vector3.zero;
+    public float parallaxFactor = 1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myStartPos = transform.localPosition;
+	}
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+		Vector3 anchorToCam = parallaxCamera.position - (worldOrigAsAnchor ? Vector3.zero : parallaxAnchor.position);
+        // set position with parallax effect
+        Vector3 shift;
+        if (onlyX)
+        {
+			shift = new Vector3(parallaxFactor * anchorToCam.x, 0, 0);
+        }
+        else
+        {
+			shift = new Vector3(parallaxFactor * anchorToCam.x, parallaxFactor * anchorToCam.y, 0);
+        }
+		// clamp
+		shift = new Vector3(Mathf.Clamp(shift.x, xLimit.x, xLimit.y), Mathf.Clamp(shift.y, yLimit.x, yLimit.y), 0);
+        transform.localPosition = myStartPos + shift;
+    }
+}
